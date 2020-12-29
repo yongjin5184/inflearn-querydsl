ac839378a1d190a1ff718312099f78954c034dc6
Kimyongjin <yongjin5184@gmail.com>
2020-12-29 13:01:02 +0900

    q 파일, lombok 테스트

--- src/test/java/com/example/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/example/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.study.querydsl;
 
+import com.example.study.querydsl.entity.Member;
+import com.example.study.querydsl.entity.QMember;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
+@Transactional
+@Commit
 class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Member member = new Member();
+        em.persist(member);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QMember findMember = QMember.member;
+
+        Member result = query
+                .selectFrom(findMember)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(member);
+        assertThat(result.getId()).isEqualTo(member.getId());
     }
 
 }
